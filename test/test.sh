12481ca5416f4d2c5d63dff87d10c727472b8482
Daniela Auriema <daniela.auriema@yahoo.com.br>
2024-12-04 21:58:09 +0100

    fix permissions

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ ! -f "./bash-test.sh" ]; then
   curl "https://raw.githubusercontent.com/danielaauriema/bash-tools/master/bash-test/bash-test.sh" > "./bash-test.sh"
+  chmod ugo+rwx "./bash-test.sh"
 fi
 
-./bash-test.sh test-config.sh
+./bash-test.sh ./test-config.sh
